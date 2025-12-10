8ad6b497f14219ff028a0de83418c23c1398f508
Strugariu Alexandru-Constantin <151936026+alexandru-strugariu15@users.noreply.github.com>
2025-12-10 07:38:26 +0000

    Materials

---
+++ summerschool_nxp/Lab/day3/Day3_testing/tests/test_memcopy.c
@@ -0,0 +1,13 @@
+
+#include <tests/test_functions.h>
+#include <string.h>
+#include <test_utils.h>
+
+
+testStatus_t test_memcopy(memcpy_func FUT, uint8_t *error_message, const void *test_params)
+{
+	/* TODO: Identify scenarios and add tests */
+
+	return STATUS_PASS;
+}
+
